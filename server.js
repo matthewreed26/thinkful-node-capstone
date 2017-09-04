ec1fd0e3f846c94660ceb8b333c898858325a410
matthewreed26 <matthewreed26@gmail.com>
2017-09-03 21:42:19 -0400

    added thinkful provided security measures to get most of the way there

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
+const passport = require('passport')
+
+const {router: usersRouter} = require('./users');
+const {router: authRouter, basicStrategy, jwtStrategy} = require('./auth');
 
 // Mongoose internally uses a promise-like object,
 // but its better to make Mongoose use built in es6 promises
@@ -14,18 +20,36 @@ const acronymsRouter = require('./acronymsRouter');
 
 // log the http layer
 app.use(morgan('common'));
+// app.all('/*', function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers',
+//   'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true })); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers',
-  'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
 
-app.use('/acronyms', acronymsRouter);
+app.use(passport.initialize());
+passport.use(basicStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
+
+// A protected endpoint which needs a valid JWT to access it
+app.get('/api/protected',
+    passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+        return res.json({
+            data: 'rosebud'
+        });
+    }
+);
+
+app.use('/api/acronyms', acronymsRouter);
 
 let server;
 
